1a1089ed735e9ff08fddad95f085a836f48ba623
OtakuNeo-Dev <danielgliddon1957@gmail.com>
2021-08-20 16:15:10 +0700

    Arrangements

---
+++ features/reply.js
@@ -0,0 +1,9 @@
+client.on("message", (message) => {
+  // Reply when Pinged
+  if (message.mentions.users.has(client.user.id) && !message.author.bot) {
+    message.reply(
+      "Hello! My prefix is `=`! If you need help, type `=help`! ;3"
+    );
+    return;
+  }
+});
